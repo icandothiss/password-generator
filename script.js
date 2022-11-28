9b2a9af4967e1dbe82b9554e786466eb3f97786f
oussama <oussama@gmail.com>
2022-11-28 15:14:02 +0100

    first commit

--- script.js
+++ script.js
@@ -1,97 +1,16 @@
-const generate = document.getElementById("generate");
-const length = document.getElementById("length");
-const upperCase = document.getElementById("uppercase");
-const lowerCase = document.getElementById("lowercase");
-const numbers = document.getElementById("numbers");
-const symbols = document.getElementById("symbols");
-const result = document.getElementById("result");
-const clipboard = document.getElementById("clipboard");
-// Generator functions
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*(){}[]=<>/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-function getRandom() {
-  let allChars = [
-    { key: getRandomUpper(), value: "upper" },
-    { key: getRandomLower(), value: "lower" },
-    { key: getRandomNumber(), value: "number" },
-    { key: getRandomSymbol(), value: "symbol" },
-  ];
-  let deleteIndex;
-
-  if (!upperCase.checked) {
-    for (let index = 0; index < allChars.length; index++) {
-      if (allChars[index].value === "upper") {
-        deleteIndex = index;
-      }
+const counters = document.querySelectorAll(".counter");
+const speed = 200; // The lower the slower
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerHTML;
+    const inc = target / speed;
+    if (count < target) {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
     }
-    allChars.splice(deleteIndex, 1);
-  }
-  if (!lowerCase.checked) {
-    for (let index = 0; index < allChars.length; index++) {
-      if (allChars[index].value === "lower") {
-        deleteIndex = index;
-      }
-    }
-    allChars.splice(deleteIndex, 1);
-  }
-  if (!numbers.checked) {
-    for (let index = 0; index < allChars.length; index++) {
-      if (allChars[index].value === "number") {
-        deleteIndex = index;
-      }
-    }
-    allChars.splice(deleteIndex, 1);
-  }
-  if (!symbols.checked) {
-    for (let index = 0; index < allChars.length; index++) {
-      if (allChars[index].value === "symbol") {
-        deleteIndex = index;
-      }
-    }
-    allChars.splice(deleteIndex, 1);
-  }
-  let numOfChecked = [
-    upperCase.checked + lowerCase.checked + numbers.checked + symbols.checked,
-  ][0];
-  if (numOfChecked > 0) {
-    console.log("y");
-    let finalResult = allChars.map(({ key, value }) => ({ key }));
-    let exemple = finalResult[Math.floor(Math.random() * allChars.length)].key;
-    return exemple;
-  } else {
-    exemple = "";
-    return exemple;
-  }
-}
-
-generate.addEventListener("click", () => {
-  let passWordLength = length.value;
-  let resultPassword = "";
-  for (let index = 0; index < passWordLength; index++) {
-    resultPassword += getRandom();
-  }
-  result.textContent = resultPassword;
-  console.log(resultPassword);
-});
-
-clipboard.addEventListener("click", () => {
-  navigator.clipboard.writeText(result.innerText);
-  if (result.innerHTML.length > 0) {
-    alert("Copied the text: " + result.innerText);
-  } else {
-    return;
-  }
+  };
+  updateCount();
 });
